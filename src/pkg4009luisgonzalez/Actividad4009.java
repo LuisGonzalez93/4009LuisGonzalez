191f0c07b70c17fe7497c9fab1da3c35e48eb0c3
luisgonzalez93 <lg02393@gmail.com>
2019-02-22 10:37:37 +0100

    Tarea finalizada

--- src/pkg4009luisgonzalez/Actividad4009.java
+++ src/pkg4009luisgonzalez/Actividad4009.java
@@ -5,10 +5,35 @@
  */
 package pkg4009luisgonzalez;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Alumno
  */
 public class Actividad4009 {
+    public Actividad4009(){
+    
+    }
+    public String encontrarPalabraRepetida(ArrayList<String> palabras)     {
+        // Escribe tu código a continuación...
+        String palabra=null;
+        ArrayList<String> temp=new ArrayList();
+        for(int i=0;i<palabras.size();i++){
+            for(int x=0;x<temp.size();x++){
+                if(!palabras.get(i).equalsIgnoreCase(temp.get(x))){
+                    //Si es distinto...
+                    temp.add(palabras.get(i));
+                }
+                else{
+                    //Si coincide...
+                    palabra=palabras.get(i);
+                }
+            }
+        }
+    return palabra;
     
 }
+
+}
+
